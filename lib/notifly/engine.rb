f5e481a25b65346fe93f3dc8c4c6a06bbd2e2709
mauropcdias <luizmauropcdias@gmail.com>
2016-03-29 10:21:14 -0300

    inheriting rails engine

--- lib/notifly/engine.rb
+++ lib/notifly/engine.rb
@@ -1,8 +1,8 @@
 require 'jquery-rails'
 
 module Notifly
-  class Engine < ::Rails::Engine
-    # isolate_namespace Notifly
+  class Engine < Rails::Engine
+    isolate_namespace Notifly
 
     config.generators do |g|
       g.test_framework :rspec
