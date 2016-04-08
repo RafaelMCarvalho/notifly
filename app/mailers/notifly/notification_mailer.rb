37a3a11ce2f60d2f6295bcce37d3a4630899872f
mauropcdias <luizmauropcdias@gmail.com>
2016-04-08 11:10:09 -0300

    Mailer receiving subject by params

--- app/mailers/notifly/notification_mailer.rb
+++ app/mailers/notifly/notification_mailer.rb
@@ -2,20 +2,19 @@ module Notifly
   class NotificationMailer < ActionMailer::Base
     default from: Notifly.mailer_sender
 
-    def notifly(to: nil, notification_id: nil, template: nil)
+    def notifly(to: nil, notification_id: nil, template: nil, subject: nil)
       if defined? Delayed::Job or defined? Sidekiq::Worker
-        delay.send_notification(to, notification_id, template)
+        delay.send_notification(to, notification_id, template, subject)
       else
-        send_notification(to, notification_id, template).deliver
+        send_notification(to, notification_id, template, subject).deliver
       end
     end
 
     private
-      def send_notification(to, notification_id, template)
+      def send_notification(to, notification_id, template, subject)
         @notification = Notifly::Notification.find(notification_id)
         @template = template
-
-        mail to: to, subject: t("notifly.mail_subject.#{@template}")
+        mail to: to, subject: subject
       end
   end
 end
